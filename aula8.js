bf6cc601465281f4880afc03d906721fd84b790a
afraniomachado <afraniomachado7@gmail.com>
2023-05-31 22:36:22 -0300

    aula8 js

---
+++ aula8.js
@@ -0,0 +1,7 @@
+const sal = 103.4;
+
+console.log("Salario: ", sal.toFixed(2)); // toFixed() transforma pra string para formatar nas casas decimais que passamos pelo argumento.
+
+const salario = "103";
+
+console.log("Salario: ", Number(salario)); //Number() passa uma string para numero
