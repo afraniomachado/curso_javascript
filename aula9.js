3f904684b93a148b83614d53950555cf084b0728
afraniomachado <afraniomachado7@gmail.com>
2023-05-31 22:45:43 -0300

    aula9 js

---
+++ aula9.js
@@ -0,0 +1,9 @@
+function soma(item1, item2) {
+  return item1 + item2;
+}
+function subtracao(item1, item2) {
+  return item1 - item2;
+}
+
+console.log("Soma: ", soma(5, 6));
+console.log("Subtração: ", subtracao(10, 2));
