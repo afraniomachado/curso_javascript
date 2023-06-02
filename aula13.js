a829c6a89a82a43491ea4a3e155efdcbd4232bf2
afraniomachado <afraniomachado7@gmail.com>
2023-06-02 16:28:20 -0300

    aula13 js

---
+++ aula13.js
@@ -0,0 +1,12 @@
+const mensagem = "vida";
+const mensagem2 = "é bela";
+const mensagem3 = "apiuogdguphiyaspdsa asoihdid";
+
+console.log(mensagem.length);
+console.log(mensagem.concat(mensagem2));
+console.log(mensagem.substring(5, 2));
+console.log(mensagem.split(" "));
+console.log("Concatenação com EcmaScript:", `A ${mensagem} ${mensagem2}`);
+console.log("Uppercase: " + mensagem.toUpperCase());
+console.log("Lowercase: " + mensagem.toLowerCase());
+console.log("Replace: " + mensagem3.replace("apiuogdguphiyaspdsa", "maria"));
