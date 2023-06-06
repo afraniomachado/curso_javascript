4446cbd59c81fd7beaa61da3f72c6f2f46628d33
afraniomachado <afraniomachado7@gmail.com>
2023-06-06 19:18:29 -0300

    aula14 js

---
+++ aula14.js
@@ -0,0 +1,9 @@
+const numero = 4321;
+const stringA = "adasdfagfs";
+const verdadeiroOuFalso = true; //falso
+
+const arrayTest = [432, 6436, 643]; // posso adicionar novos items mas nunca mudar os que ja foram informados.
+
+arrayTest.push(324243);
+
+console.log(arrayTest);
