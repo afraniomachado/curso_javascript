2f3736a72446576b53bc8c1a53fdb7f24ca4b015
afraniomachado <afraniomachado7@gmail.com>
2023-06-06 19:38:51 -0300

    aula15 js

---
+++ aula15.js
@@ -0,0 +1,15 @@
+function teste(key) {
+  switch (key) {
+    case "Afranio":
+      console.log("Afranio aqui");
+      break;
+    case "Fatima":
+      console.log("Fatima aqui");
+      break;
+    default:
+      console.log("Valor não encontrado :/");
+      break;
+  }
+}
+
+teste("Fatima");
